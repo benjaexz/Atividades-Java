0dd3b7e5db2113f611dccd0680fb0f8418383456
Jacó <jacopimentelxd@gmail.com>
2025-09-17 17:31:15 -0400

    Alterações

---
+++ br/com/atividades/Olamundo.java
@@ -0,0 +1,13 @@
+package br.com.atividades;
+
+public class Olamundo {
+    public static void main(String[] args) {
+        String nome = "Java";
+
+        System.out.println(nome);
+
+        nome = "Cleyson";
+
+       System.out.println("Ola, mundo bem vindoo");
+    }
+}
